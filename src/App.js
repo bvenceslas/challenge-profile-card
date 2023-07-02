78eeb2946d6ed1b4a28eed4c354b507100cae8a5
Venceslas Burongu <bvenceslas@gmail.com>
2023-07-02 20:32:41 +0000

    complete project

--- src/App.js
+++ src/App.js
@@ -10,34 +10,50 @@ export default function App() {
 
 const Techno = (props) => {
   console.log("techs:", props.technologies);
-  return props.technologies.map((techs, index) => {
-    return <div key={index}>{techs}</div>;
-  });
+
+  return (
+    <div className="techno-wrapper">
+      {props.technologies.map((techs, index) => (
+        <div key={index}>
+          <div
+            className="techno"
+            style={{ backgroundColor: `#${Math.round(Math.random() * 10000)}` }}
+          >
+            {techs}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const Card = () => {
   const technologies = [
-    "HTML + CSS",
-    "Javascript",
-    "Web Design",
-    "Git and Github",
-    "React"
+    "HTML + CSS 👍🏽",
+    "Javascript 👍🏽",
+    "Web Design 👍🏽",
+    "Git and Github 👍🏽",
+    "React 👍🏽",
+    "Node 👍🏽",
   ];
 
   return (
-    <div>
+    <div className="card">
       <img
         src="https://avatars.githubusercontent.com/u/59555731?s=400&u=874ee771b540434bcc5ec33e85eff66c2abf71ad&v=4"
-        alt=""
+        alt="brain picture"
       />
-      <h2>Jonas Schmedtmann</h2>
-      <p>
-        Full-stack developer at Skilly Hub. When not coding i like to listen to
-        music, do meditation, cook (and eat), or just explore the world of
-        reading books.
-      </p>
 
-      <Techno technologies={technologies} />
+      <div className="card-text">
+        <h2>Jonas Schmedtmann</h2>
+        <p>
+          Full-stack developer at Skilly Hub. When not coding i like to listen
+          to music, do meditation, cook (and eat), or just explore the world of
+          reading books.
+        </p>
+
+        <Techno technologies={technologies} />
+      </div>
     </div>
   );
 };
